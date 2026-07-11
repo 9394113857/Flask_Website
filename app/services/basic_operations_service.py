09af1b914e1d15263f0c7ff030aed66de2c7c05e
Raghunadh_9394113857 <barlapudiraghunadh9@gmail.com>
2026-07-11 12:06:02 +0530

    Mering into main now

--- app/services/basic_operations_service.py
+++ app/services/basic_operations_service.py
@@ -11,22 +11,32 @@ class BasicOperationsService:
 
     @staticmethod
     def add(number1, number2):
+
         return number1 + number2
+
 
     @staticmethod
     def subtract(number1, number2):
+
         return number1 - number2
+
 
     @staticmethod
     def multiply(number1, number2):
+
         return number1 * number2
+
 
     @staticmethod
     def divide(number1, number2):
 
         if number2 == 0:
-            return "Cannot divide by zero." 
+
+            raise ZeroDivisionError(
+                "Cannot divide by zero."
+            )
 
         return number1 / number2
-    
+
+
 # Notice that Division includes divide-by-zero validation.
